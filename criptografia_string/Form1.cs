2ee4203a50c41b9b6fe3ed5a31d50c92e4bace0b
diegofelipe01 <50500419+diegofelipe01@users.noreply.github.com>
2020-05-29 13:25:53 -0300

    Add files via upload

---
+++ criptografia_string/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace AOJDIASJDSAIFJAISOFJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Criptografar()
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Padding = PaddingMode.Zeros;
+            byte[] key = ASCIIEncoding.ASCII.GetBytes("swqa23ue");
+            byte[] iv = ASCIIEncoding.ASCII.GetBytes("1wasqw12");
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, DES.CreateEncryptor(key, iv), CryptoStreamMode.Write);
+            StreamWriter streamWriter = new StreamWriter(cryptoStream);
+            streamWriter.Write(textBox1.Text);
+            streamWriter.Flush();
+            cryptoStream.FlushFinalBlock();
+            streamWriter.Flush();
+            return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+        }
+
+        private string Descripto()
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Padding = PaddingMode.Zeros;
+            byte[] key = ASCIIEncoding.ASCII.GetBytes("swqa23ue");
+            byte[] iv = ASCIIEncoding.ASCII.GetBytes("1wasqw12");
+            MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(textBox2.Text));
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, DES.CreateDecryptor(key, iv), CryptoStreamMode.Read);
+            StreamReader streamReader = new StreamReader(cryptoStream);
+            return streamReader.ReadToEnd();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  
+        {
+            textBox2.Text = Criptografar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Descripto(); 
+        }
+    }
+     
+}
